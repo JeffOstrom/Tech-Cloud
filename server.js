4f1e5684ea227666eaef373ee150af4ca5008a12
Jeff Ostrom <j.ostrm@gmail.com>
2018-01-21 18:21:12 -0600

    initial layout server.js/npm

---
+++ server.js
@@ -0,0 +1,8 @@
+//Dependencies npm packages
+var express = require("express");
+var exphbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+//Require request and cheerio for scraping
+var cheerio = require("cheerio");
+var request = require("request");

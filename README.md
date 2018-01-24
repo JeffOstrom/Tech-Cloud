8db38060db0853796d58bfb29826796c0afab7ea
Jeff Ostrom <j.ostrm@gmail.com>
2018-01-24 14:34:14 -0600

    server.js/routes layout

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# mongo-scrape
+Synopsis
+-------------------------------------------------------------------------------------
+
+- Discover the news daily and save your top favorite articles 
+
+- This web app gives users the freedom to scrape current news from website
+
+- Go read some news! Check it out! https://jeffostrom.github.io/mongo-scrape/
+
+Built With
+-----------------------------------------------------------------------------
+- MongoDb
+
+- Mongoose
+
+- Express 
+
+- Handlebars 
+
+- Cheerio
+
+- Robo
+
+Resources
+-----------------------------------------------------------------------------
+
+Contributors
+-----------------------------------------------------------------------------
+
+- Jeffrey Ostrom
+
+License
+-----------------------------------------------------------------------------
+- None
+
+Acknowledgments
+-------------------------------------------------------------------------------------
+- None
+
+
+
+
--- server.js
+++ server.js
@@ -3,6 +3,57 @@ var express = require("express");
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var mongojs = require('mongojs');
 //Require request and cheerio for scraping
 var cheerio = require("cheerio");
 var request = require("request");
+// Set mongoose to leverage built in JavaScript 
+mongoose.Promise = Promise;
+
+const PORT = process.env.PORT || 3000;
+
+// Initialize Express
+const APP = express();
+
+// Use body parser
+APP.use(bodyParser.urlencoded({extended: false}));
+
+// Make public a static dir
+APP.use(express.static("public"));
+
+// Set Handlebars.
+APP.engine("handlebars", EXPHBS({ defaultLayout: "main" }));
+APP.set("view engine", "handlebars");
+
+
+// Database configuration with mongoose
+if(process.env.MONGODB_URI)
+{
+	mongoose.connect(process.env.YOUR MONGODB_URI HERE);
+}
+else
+{
+	mongoose.connect("mongodb://localhost/YOUR DB NAME HERE");
+}	
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// Routes
+// ======
+var routes = require("./controllers/routes.js");
+app.use("/", routes);
+
+// Listen on port 3000
+APP.listen(PORT, function() {
+  console.log("App running on port", PORT);
+});
